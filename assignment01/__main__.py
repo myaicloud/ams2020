b4aa81324ef8b8a19db551d5333261aeb6591032
Andi <andi@ahlbach.net>
2020-05-01 13:59:08 +0200

    assignment01

---
+++ assignment01/__main__.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def sample_normal_distributed_value(samples_to_average=12):
+    print('Implementation here')
+
+def main():
+    print('I am assignment 1.')
+    sample_normal_distributed_value()
+
+if __name__ == '__main__':
+    main()
